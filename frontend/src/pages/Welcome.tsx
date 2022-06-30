35caee6fb9dbea9ccf1b8ade817db0529819103b
Wesleson22 <wesllesonsowza@gmail.com>
2022-06-29 21:33:19 -0300

    Tele 2 concluida

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,31 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
+//Armazenando os estados co useState..
 import {SafeAreaView, Text, View, Image, TouchableOpacity, StyleSheet } from "react-native"; 
 //TouchableOpacity esse ele faz um feito de opacidade quando ele eh precionado.
 import logoIgm from '../assets/logo.png';
+import { Button } from "../components/Button";
 import colors from "../styles/colors";
 
 export function Welcome(){
+    const [visible, setVisible] = useState(false);
+    
+    function handleVisibility(){
+        setVisible(true)
+        //essa função eh responsável por seta a image da logo quando o 
+        //Button for presionado.
+    }
+
     return(
         <SafeAreaView style={styles.container}>
             
         
             <Text style={styles.title}>
-            Gerencie {'\n'}eu dia a dia de {'\n'}forma fácil.
+            Gerencie {'\n'}seu dia a dia de {'\n'}forma fácil.
             </Text>
-
+            {
+            visible &&
             <Image source={logoIgm} style={styles.image}/>
-
+            }
             <Text style={styles.subtitle}>
             Nós cuidamos de lembrar você
             sempre que precisar.
             </Text>
 
-
-            <TouchableOpacity style={styles.button}>
-                <Text>
-                    >
-                </Text>
-            </TouchableOpacity>
+            <Button title=">"/>
         </SafeAreaView>
     )
 
@@ -62,7 +68,7 @@ const styles = StyleSheet.create({
         borderRadius:16,
         marginBottom:10,
         height:56,
-        width: 56
+        paddingHorizontal: 10
         
     },
 
