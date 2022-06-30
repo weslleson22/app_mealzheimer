0eea5ac134746a6eb7fba8406e2c457c16d0747c
Wesleson22 <wesllesonsowza@gmail.com>
2022-06-30 01:02:16 -0300

    Tela UserIndentification 39 m

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import {Text, View, StyleSheet} from 'react-native';
 import { Welcome } from "./src/pages/Welcome";
+import { useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from "@expo-google-fonts/jost";
+import AppLoading from 'expo-app-loading';
+import { UserIdentification } from "./src/pages/UserIdentification";
+/*
+expo install expo-app-loading vai ajuda a segurar a tela de splash até a fonte ser carregada.
+
+*/
 export default function App(){
 
-  return(
-   <Welcome/>
+ const [fonstsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+  if (!fonstsLoaded){
+    return  <AppLoading/>
+  
+  }
 
+  return(
+   //<Welcome/>
+    <UserIdentification/>
   )
 }
 
---
+++ frontend/src/pages/UserIdentification.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { 
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View
+} from "react-native";
+
+export function UserIdentification(){
+
+    return(
+        <SafeAreaView style={styles.containe}>
+        <View>
+            <View>
+                
+            </View>
+        </View>
+        </SafeAreaView>
+    )
+}
+const styles = StyleSheet.create({
+    containe: {
+
+    }
+})
