60515b0b4ab9da6c38fb7051b5277bff36215e42
Wesleson22 <wesllesonsowza@gmail.com>
2022-07-30 08:26:37 -0300

    Salvando no bd os pontos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,51 @@
 import express, { request, response } from "express";
+import knex from "./database/connection";
 
-const routes = express.Router();
+const routes = express.Router(); 
 
-routes.get('/',(request, response)=>{
-    return response.json({message: 'Hello World'});
+routes.get('/items', async(request, response)=>{
+
+    const items = await knex('items').select('*');
+    const serializedItems = items.map(item=>{
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        }; 
+    }); 
+
+    return response.json(serializedItems);
 
 });
+ 
+    routes.post('/points', async (request, response)=>{
+        const {
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf,
+            items
+        } = request.body;
+        await knex('points').insert({
+            
+            image: 'image-fake',
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf,
+            
+       });
+       return response.json({success: true});
+    });
+
+  
+
+
 
 export default routes;
